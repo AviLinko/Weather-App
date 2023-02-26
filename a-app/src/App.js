86ea9650eee7671e1dfd6277b20d4260928d8747
Avraham Kaplan <99396617+AviLinko@users.noreply.github.com>
2023-02-26 15:39:48 +0200

    .

--- a-app/src/App.js
+++ a-app/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import {ThemeProvider} from '@mui/material/styles'
+import {theme} from './themes/theme'
+import {Routes , Route} from 'react-router-dom'
+import {Dashboard, Map, Saves} from './pages/allPages'
+import TopNav from './components/TopNav'
+import WeatherDisplay from './components/WeatherDisplay'
+import LeftNav from './components/LeftNav'
+import Footer from './components/Footer'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <TopNav />
+    </div>
   )
 }
 
